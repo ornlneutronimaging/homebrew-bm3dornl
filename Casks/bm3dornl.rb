4dd66a6e8ea588d1692ea17e7444de75b871bd43
Chen Zhang <chenzhang8722@gmail.com>
2026-01-30 12:37:43 -0500

    Add postflight to remove quarantine attribute from unsigned app

--- Casks/bm3dornl.rb
+++ Casks/bm3dornl.rb
@@ -12,6 +12,15 @@ cask "bm3dornl" do
 
   app "bm3dornl.app"
 
+  postflight do
+    system_command "/bin/echo",
+                   args: ["Removing quarantine attribute from bm3dornl.app (app is not code-signed)..."]
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/bm3dornl.app"]
+    system_command "/bin/echo",
+                   args: ["Done. bm3dornl.app is ready to use."]
+  end
+
   zap trash: [
     "~/Library/Preferences/com.ornl.bm3dornl.plist",
     "~/Library/Saved Application State/com.ornl.bm3dornl.savedState",
